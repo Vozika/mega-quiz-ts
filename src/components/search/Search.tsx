3938d51edf1d46921c676b0c4b0cfef199d0ff17
Sergey Vozika <metasoma@yandex.ru>
2022-12-24 20:47:11 +0300

    Presumably Finished Search

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Zoom from "@mui/material/Zoom";
 import { useSelector } from "react-redux";
 import { RootStore, useAppDispatch } from "../../store";
 import { setSearchWord } from "../../features/utilities/utilitiesSlice";
+import { Typography } from "@mui/material";
 
 interface Props {
   backToStart: () => void;
@@ -15,6 +18,10 @@ const Search = ({ backToStart }: Props) => {
   const { interfaceText } = useSelector((store: RootStore) => store.options);
   const { data } = useSelector((store: RootStore) => store.engine);
   const { searchWord } = useSelector((store: RootStore) => store.utilities);
+
+  function filterItem(option: string) {
+    return option.toLowerCase().includes(searchWord.toLowerCase());
+  }
 
   return (
     <Stack
@@ -29,38 +36,71 @@ const Search = ({ backToStart }: Props) => {
         justifyContent: "center",
       }}
     >
-      <TextField
-        label={interfaceText.SEARCH}
-        id="search"
-        defaultValue=""
-        variant="filled"
-        onChange={(event) => {
-          dispatch(setSearchWord(event.target.value));
-        }}
+      <Box
+        component="form"
+        noValidate
+        autoComplete="off"
         sx={{
           width: "100%",
         }}
-      />
-      <br />
+      >
+        <Typography variant="h5" mb={2}>
+          Type a country or a capital in English
+        </Typography>
+        <TextField
+          label={interfaceText.SEARCH}
+          id="search"
+          defaultValue=""
+          variant="filled"
+          onChange={(event) => {
+            dispatch(setSearchWord(event.target.value));
+          }}
+          sx={{
+            width: "100%",
+          }}
+        />
+      </Box>
+
       {data
         .filter((item) => {
-          if (
-            item.name.translations.en
-              .toLowerCase()
-              .includes(searchWord.toLowerCase())
-          ) {
+          if (filterItem(item.name.translations.en)) {
             return item;
-          } else if (
-            item.capital.translations.en
-              .toLowerCase()
-              .includes(searchWord.toLowerCase())
-          ) {
+          } else if (filterItem(item.capital.translations.en)) {
             return item;
           }
         })
         .map((item) => {
           if (searchWord !== "" && searchWord.length > 2) {
-            return item.name.translations.en;
+            return (
+              <>
+                <Zoom in={true} timeout={450}>
+                  <Box
+                    component="img"
+                    src={"flag/" + item.id + ".svg"}
+                    sx={{
+                      width: {
+                        xs: "100%",
+                        sm: 320,
+                        xl: 420,
+                      },
+                      height: {
+                        xs: "auto",
+                        sm: "auto",
+                        xl: "auto",
+                      },
+                      p: 0,
+                      mb: 3,
+                      mt: 3,
+                      ml: 0,
+                      mr: 0,
+                    }}
+                  />
+                </Zoom>
+                <Typography>
+                  {item.name.translations.en} / {item.capital.translations.en}
+                </Typography>
+              </>
+            );
           }
         })}
       <br />
